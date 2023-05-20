7d2fc4e7120c127076d72d19e1459472e3ad20ff
eearly01 <elijah.early@gmail.com>
2023-05-20 16:35:44 -0500

    added message component to the bottom of the page

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -3,15 +3,19 @@ import Navbar from './layouts/NavbarAndFooter/Navbar';
 import './App.css';
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
 import { Carousel } from './layouts/HomePage/Carousel';
+import { Heros } from './layouts/HomePage/Heros';
+import { LibraryServices } from './layouts/HomePage/LibraryServices';
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <ExploreTopBooks />
-      <Carousel />
-    </div>
-  );
+	return (
+		<div>
+			<Navbar />
+			<ExploreTopBooks />
+			<Carousel />
+			<Heros />
+			<LibraryServices />
+		</div>
+	);
 }
 
 export default App;
