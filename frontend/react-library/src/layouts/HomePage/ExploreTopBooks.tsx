12d906a35430bba622746f496643ee16c90e580b
eearly01 <elijah.early@gmail.com>
2023-05-20 15:38:08 -0500

    finished basic carousel

---
+++ frontend/react-library/src/layouts/HomePage/ExploreTopBooks.tsx
@@ -0,0 +1,15 @@
+export const ExploreTopBooks = () => {
+    return (
+        <div className="p-5 mb-4 bg-dark header">
+            <div className="container-fluid py-5 text-white d-flex justify-content-center align-items-center">
+                <div>
+                    <h1 className="display-5 fw-bold">
+                        <p className="col-md-8 fs-4">
+                            <a type='button' href="#" className="btn main-color btn-lg text-white">Explore Top Books</a>
+                        </p>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    )
+}
